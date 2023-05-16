f372e3e93d3c02010a885cd837cb124df8dfd467
Aki Wu <wuminzhe@gmail.com>
2023-05-16 15:33:17 +0800

    remove build.sh

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-bundle exec rake db:create
-bundle exec rake db:migrate
-bundle exec rake db:seed
--- bin/start.sh
+++ bin/start.sh
@@ -5,8 +5,8 @@ set -o errexit
 # throw errors if Gemfile has been modified since Gemfile.lock
 bundle config --global frozen 1
 
+bundle exec rails db:prepare
 bundle exec rake assets:clean
 bundle exec rake assets:precompile
-bundle exec rake db:migrate
 
 bundle exec puma -C config/puma.rb
